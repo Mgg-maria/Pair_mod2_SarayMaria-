bc59b01bd89e98824040bf91356762c02606ca1f
Maria G <mgg.guiso@gmail.com>
2025-02-25 11:33:56 +0100

    pair modificacion e insercion de datos

--- EJERCICIO PAIR tienda_zapatillas.sql
+++ EJERCICIO PAIR tienda_zapatillas.sql
@@ -56,7 +56,64 @@ CREATE TABLE facturas (
 
 );
     
+    ALTER TABLE tabla_zapatillas
+    ADD COLUMN marca VARCHAR(45) NOT NULL;
+    
+    
+ ALTER TABLE tabla_zapatillas
+ ADD COLUMN talla INT NOT NULL;
+    
+ALTER TABLE empleados
+MODIFY COLUMN salario FLOAT NOT NULL;
+
+ALTER TABLE clientes
+DROP COLUMN pais;
+
+ALTER TABLE facturas
+ADD COLUMN total FLOAT;
 
 
-  
+INSERT INTO tabla_zapatillas(id_zapatilla,modelo,color,marca,talla)
+VALUES (1,"XQYUN","Negro","Nike",42),
+	
+INSERT INTO tabla_zapatillas(id_zapatilla,modelo,color,marca,talla)  
+VALUES (2,"UOPMN","Rosas","Nike",39),
+	(3,"OPNYT","Verdes","Adidas",35);
+    
+SELECT *
+FROM tabla_zapatillas;
+
+
+
+INSERT INTO empleados (id_empleado,nombre,tienda,salario,fecha_incorporacion)
+VALUES (1,"Laura","Alcobendas",25897,"2010-09-03"),
+	(2,"Maria","Sevilla",0.0,"2001-04-11"),
+    (3,"Ester","Oviedo",30165.98,"2000-11-29");
+
+SELECT * 
+FROM empleados;
  
+ INSERT INTO clientes (id_cliente,nombre,num_telefono,email,direccion,ciudad,provincia,cp)
+ VALUES (1,"Monica","1234567289","monica@email.com","Calle Felicidad","Mostoles","Madrid","28176"),
+	(2,"Lorena","289345678","lorena@email.com","Calle Alegria","Barcelona","Barcelona","12346"),
+    (3,"Carmen","298463759","carmen@email.com","Calle del Color","Vigo","Pontevedra","23456");
+    
+SELECT * 
+FROM clientes;
+
+INSERT INTO facturas (id_factura,num_factura,fecha,id_zapatilla,id_empleado,id_cliente,total)
+VALUES(1,"123","2001-12-11",1,2,1,54.98),
+	(2,"1234","2005-05-23",1,1,3,89.91),
+    (3,"12345","2015-09-18",2,3,3,76.23);
+    
+SELECT *
+FROM facturas;
+
+UPDATE tabla_zapatillas
+SET color="Amarillas"
+WHERE color="Rosas"
+
+SELECT *
+FROM tabla_zapatillas;
+
+SET SQL_SAFE_UPDATES = 0
